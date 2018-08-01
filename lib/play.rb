2439476dfdfbadc17747e3f264bce15bf7cb4770
ipino0128@gmail.com <ipino0128@gmail.com>
2018-08-01 21:03:29 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  until turn == 9
-    move(board, index)
-    display_board(board)
-turn += 1
+  until valid_move?(board, index) == true
+    turn(board)
 end
 end
